66c973d0713624136f8a6f09269eb070526f70e5
dfm18 <dfm18@users.noreply.github.com>
2025-01-27 18:45:39 -0500

    calculator conundrum

---
+++ src/test/java/com/dfm18/exercism/calculatorconundrum/CalculatorConundrumTest.java
@@ -0,0 +1,80 @@
+package com.dfm18.exercism.calculatorconundrum;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class CalculatorConundrumTest {
+
+    @Test
+    @DisplayName("The calculate method returns the correct result when adding small operands")
+    public void additionWithSmallOperands() {
+        assertThat(new CalculatorConundrum().calculate(22, 25, "+")).isEqualTo("22 + 25 = 47");
+    }
+
+    @Test
+    @DisplayName("The calculate method returns the correct result when adding large operands")
+    public void additionWithLargeOperands() {
+        assertThat(new CalculatorConundrum().calculate(378_961, 399_635, "+")).isEqualTo("378961 + 399635 = 778596");
+    }
+
+    @Test
+    @DisplayName("The calculate method returns the correct result when multiplying small operands")
+    public void multiplicationWithSmallOperands() {
+        assertThat(new CalculatorConundrum().calculate(3, 21, "*")).isEqualTo("3 * 21 = 63");
+    }
+
+    @Test
+    @DisplayName("The calculate method returns the correct result when multiplying large operands")
+    public void multiplicationWithLargeOperands() {
+        assertThat(new CalculatorConundrum().calculate(72_441, 2_048, "*")).isEqualTo("72441 * 2048 = 148359168");
+    }
+
+    @Test
+    @DisplayName("The calculate method returns the correct result when dividing small operands")
+    public void divisionWithSmallOperands() {
+        assertThat(new CalculatorConundrum().calculate(72, 9, "/")).isEqualTo("72 / 9 = 8");
+    }
+
+    @Test
+    @DisplayName("The calculate method returns the correct result when dividing large operands")
+    public void divisionWithLargeOperands() {
+        assertThat(new CalculatorConundrum().calculate(1_338_800, 83_675, "/")).isEqualTo("1338800 / 83675 = 16");
+    }
+
+    @Test
+    @DisplayName("The calculate method throws IllegalOperationException when passing invalid operation")
+    public void throwExceptionForUnknownOperation() {
+        String invalidOperation = "**";
+        String expectedMessage = "Operation '" + invalidOperation + "' does not exist";
+        assertThatExceptionOfType(IllegalOperationException.class)
+                .isThrownBy(() -> new CalculatorConundrum().calculate(3, 78, invalidOperation))
+                .withMessage(expectedMessage);
+    }
+
+    @Test
+    @DisplayName("The calculate method throws IllegalArgumentException when passing null operation")
+    public void throwExceptionForNullAsOperation() {
+        assertThatExceptionOfType(IllegalArgumentException.class)
+                .isThrownBy(() -> new CalculatorConundrum().calculate(66, 65, null))
+                .withMessage("Operation cannot be null");
+    }
+
+    @Test
+    @DisplayName("The calculate method throws IllegalArgumentException when passing empty operation")
+    public void throwExceptionForAnEmptyStringOperation() {
+        assertThatExceptionOfType(IllegalArgumentException.class)
+                .isThrownBy(() -> new CalculatorConundrum().calculate(34, 324, ""))
+                .withMessage("Operation cannot be empty");
+    }
+
+    @Test
+    @DisplayName("The calculate method throws IllegalOperationException when dividing by zero")
+    public void throwExceptionForDivisionByZero() {
+        assertThatExceptionOfType(IllegalOperationException.class)
+                .isThrownBy(() -> new CalculatorConundrum().calculate(33, 0, "/"))
+                .withMessage("Division by zero is not allowed").withCauseInstanceOf(ArithmeticException.class);
+    }
+}
